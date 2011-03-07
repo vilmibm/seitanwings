a96e39d811aad1d9bfac44bb4bd6bb07f3d44d4b
nathanielksmith <nathanielksmith@gmail.com>
2011-03-06 22:54:53 -0500

    cleanup, more robust link scraping

--- seitanwings/wings.py
+++ seitanwings/wings.py
@@ -21,26 +21,21 @@ url_queue = []
 start_url = 'http://www.google.com/Top/Arts/'
 
 url_queue.append(start_url)
-iterations = 100
+iterations = 10
 
 opener = urllib2.build_opener()
 opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
 
-protocol = 'http://'
-
 while len(url_queue) > 0 and iterations > 0:
     url = url_queue.pop(0)
-    if seen_urls.get(url, False):
-        print "Already seen %s, continuing" % url
-        continue
 
     try:
         data = opener.open(url).read()
     except (urllib2.HTTPError, ValueError, urllib2.URLError):
         print "Got an HTTP error for %s, continuing" % url
         continue
-    root = urlparse.urlparse(url).netloc
-    soup = BeautifulSoup(data)
+    parsed_url = urlparse.urlparse(url)
+    (scheme, root, path) = (parsed_url.scheme, parsed_url.netloc, parsed_url.path)
     # parse content
     soup = BeautifulSoup(data)
     page = {
@@ -50,10 +45,20 @@ while len(url_queue) > 0 and iterations > 0:
     pages.insert(page)
     seen_urls[url] = True
     for link in soup.findAll('a'):
-        if link.has_key('href'):
-            link = link['href']
-            if not link.startswith('http://'):
-                link = protocol + root + link
+        if not link.has_key('href'):
+            continue
+
+        link = link['href']
+        base = scheme + '://' + root
+        if link.startswith('/'):
+            link = urlparse.urljoin(base, link)
+        elif link.startswith('http://'):
+            pass
+        else:
+            absolute = urlparse.urljoin(base, path)
+            link = urlparse.urljoin(absolute, link)
+
+        if not seen_urls.get(link):
             url_queue.append(link)
     iterations -= 1
 
